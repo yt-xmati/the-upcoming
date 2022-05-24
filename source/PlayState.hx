44f4e66d7ab598c87c9bb74e485933064d72d8e7
xmati <93664892+yt-xmati@users.noreply.github.com>
2022-05-24 22:01:19 +0200

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4349,17 +4349,12 @@ class PlayState extends MusicBeatState
  			//health icon bounce but epic
  			if (curBeat % gfSpeed == 0) {
  				curBeat % (gfSpeed * 2) == 0 ? {
- 					iconP1.scale.set(1.1, 0.8);
- 					iconP2.scale.set(1.1, 1.3);
-
- 					FlxTween.angle(iconP1, -15, 0, Conductor.crochet / 1300 * gfSpeed, {ease: FlxEase.quadOut});
- 					FlxTween.angle(iconP2, 15, 0, Conductor.crochet / 1300 * gfSpeed, {ease: FlxEase.quadOut});
+ 					iconP1.scale.set(1.1, 0.6);
+ 					iconP2.scale.set(1.1, 1.5);
  				} : {
- 					iconP1.scale.set(1.1, 1.3);
- 					iconP2.scale.set(1.1, 0.8);
-
- 					FlxTween.angle(iconP2, -15, 0, Conductor.crochet / 1300 * gfSpeed, {ease: FlxEase.quadOut});
- 					FlxTween.angle(iconP1, 15, 0, Conductor.crochet / 1300 * gfSpeed, {ease: FlxEase.quadOut});
+ 					iconP1.scale.set(1.1, 1.5);
+ 					iconP2.scale.set(1.1, 0.6);
+					
  				}
 
  				FlxTween.tween(iconP1, {'scale.x': 1, 'scale.y': 1}, Conductor.crochet / 1250 * gfSpeed, {ease: FlxEase.quadOut});
